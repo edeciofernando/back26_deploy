abea1e2ff79f601d115c8a15016e105c9d7ba487
Edécio Fernando Iepsen <edeciofernando@gmail.com>
2026-05-02 14:22:01 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import { Combustiveis } from '@prisma/client'
+
+import routesMarcas from './routes/marcas'
+import routesCarros from './routes/carros'
+import routesClientes from './routes/clientes'
+import routesLogin from './routes/login'
+import routesPropostas from './routes/propostas'
+import routesDashboard from './routes/dashboard'
+import routesAdminLogin from './routes/adminLogin'
+import routesAdmins from './routes/admins'
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/marcas", routesMarcas)
+app.use("/carros", routesCarros)
+app.use("/clientes", routesClientes)
+app.use("/clientes/login", routesLogin)
+app.use("/propostas", routesPropostas)
+app.use("/dashboard", routesDashboard)
+app.use("/admins/login", routesAdminLogin)
+app.use("/admins", routesAdmins)
+
+app.get('/', (req, res) => {
+  res.send('API: Revenda de Veículos')
+})
+
+app.get('/combustiveis', (req, res) => {
+  const combustiveis = Object.keys(Combustiveis)
+  res.status(200).json(combustiveis)
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`)
+})
